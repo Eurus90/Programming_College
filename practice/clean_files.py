45068ba12ca88f15c397c2564f7d621cce21adc6
Lorcan <lorcan.moore1@gmail.com>
2015-02-25 02:37:40 +0000

    tidying the print in clean

--- practice/clean_files.py
+++ practice/clean_files.py
@@ -10,13 +10,11 @@ def main():
         file_ls = []
         file_ls = os.listdir()
 
-        print(file_ls)
         ext_ls = ['i', 'v', '4']
 
         for name in file_ls:
             fold_nm = name
             newdir = str(orgdir + '/' + fold_nm)
-            print(newdir)
             try:
                 os.chdir(newdir)
                 newfl_ls = []
@@ -24,6 +22,7 @@ def main():
                 for fl_nm in newfl_ls:
                     if fl_nm[-1] in ext_ls:
                         os.rename(newdir + '/' + fl_nm, orgdir + '/' + fl_nm)
+                        print('\n\n-- ', fl_nm, '\n\n FROM  :', newdir, '\n\n   TO  :', orgdir, '\n\n')
                     else:
                         continue
 
