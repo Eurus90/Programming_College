131047da2539aeff2a491661df8bc9f528aeaa81
Lorcan <lorcan.moore1@gmail.com>
2015-02-25 01:01:07 +0000

    still updating clean

--- practice/clean_files.py
+++ practice/clean_files.py
@@ -4,15 +4,27 @@ import string
 
 def main():
     try:
-        dir_path = input("Please input the dir :  ")
-        os.chdir(dir_path)
+        orgdir = input("Please input the dir :  ")
+        os.chdir(orgdir)
 
         file_ls = []
         file_ls = os.listdir()
 
         print(file_ls)
+        ext_ls = ['i', 'v', '4']
 
-
+        for name in file_ls:
+            fold_nm = name
+            newdir = str(orgdir + '/' + fold_nm)
+            print(newdir)
+            os.chdir(newdir)
+            newfl_ls = []
+            newfl_ls = os.listdir()
+            for fl_nm in newfl_ls:
+                if fl_nm[-1] in ext_ls:
+                    os.rename(newdir + '/' + fl_nm, orgdir + fl_nm)
+                else:
+                    continue
 
     except OSError as e:
         print(e)
